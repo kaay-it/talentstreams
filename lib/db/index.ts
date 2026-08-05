d2d1a0a9ceaca43843f0384deb2fd9ffaf3c75ba
Aleksey Kalinin <kaay@it-improve.ru>
2026-08-05 16:33:09 +0500

    fix neon

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -8,5 +8,18 @@ if (process.env.NODE_ENV !== "production") {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 }
 
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql, { schema })
+function createDb() {
+  const url = process.env.DATABASE_URL
+  if (!url) throw new Error("DATABASE_URL is not set")
+  return drizzle(neon(url), { schema })
+}
+
+type Db = ReturnType<typeof createDb>
+let _db: Db | undefined
+
+export const db: Db = new Proxy({} as Db, {
+  get(_, prop) {
+    if (!_db) _db = createDb()
+    return (_db as unknown as Record<string | symbol, unknown>)[prop]
+  },
+})
